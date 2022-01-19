94b5b07bce10f079b0e9a13aa7f858db1cd1b32e
tyassusanto <nasusayt@gmail.com>
2022-01-19 23:42:24 +0700

    add navbar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>}/>
+        {/* <Route path="/" element={<Login/>}/> */}
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Input from '../Input';
+import Button from '../Button';
+import styles from './navbar.module.css';
+// import Logo from '../../../public/images/shopping-bag 1';
+import { FiShoppingCart } from 'react-icons/fi';
+
+
+
+
+const Navbar = () => {
+  return (
+      <div className={`${styles.headerbody}  container-fluid`}>
+          <div className={`container bg-white d-flex py-4 align-items-center`}>
+              <div className="left w-25 text-primary"><h4>Blanja</h4></div>
+              <div className="mid w-50"><Input placeholder='Search' className={`${styles.src} w-75 px-3`}/></div>
+              <div className="right d-flex w-25 justify-content-end">
+                  <div className="cart-icon fs-4 me-5 text-secondary"><FiShoppingCart/></div>
+                  <div className="button">
+                        <Button className={`${styles.buttonLogin} btn bg-primary text-white`}>Login</Button>
+                        <Button className={`${styles.buttonSignup} btn bg-white text-secondary ms-3`}>Signup</Button>
+                  </div>
+              </div>
+          </div>
+      </div>
+  );
+};
+
+export default Navbar;
