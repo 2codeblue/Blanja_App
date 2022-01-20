4e0b01976e321f697df918f74c7829a2c4b85844
tyassusanto <nasusayt@gmail.com>
2022-01-20 18:00:16 +0700

    merge

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
-import HomeCarousel from './components/HomeCarousel';
 import Login from './pages/Auth/Login';
 import HomePage from './pages/Home';
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<Login/>}/>
-    //   </Routes>
-    // </BrowserRouter>
-    // <HomeCarousel/>
-    <HomePage/>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/home" element={<HomePage/>}/>
+      </Routes>
+    </BrowserRouter>
+
+    );
 }
 
 export default App;
