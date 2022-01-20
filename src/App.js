144321db4fe952ccba7cdcefa91fe34000ad8098
tyassusanto <nasusayt@gmail.com>
2022-01-20 17:25:54 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
+import HomeCarousel from './components/HomeCarousel';
 import Login from './pages/Auth/Login';
+import HomePage from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<Login/>}/> */}
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Login/>}/>
+    //   </Routes>
+    // </BrowserRouter>
+    // <HomeCarousel/>
+    <HomePage/>
   );
 }
 
---
+++ src/components/MainHome/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainHome = ({children}) => {
+  return (
+    <div className="main-wrapper container-fluid">
+        {children}
+    </div>
+    )
+};
+
+export default MainHome;
