8bfff066f8f10aecf7b1c2164e7f3f7fe4acc587
dindachyfr <dindachyfr@gmail.com>
2022-01-20 19:03:15 +0700

    first

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Login from './pages/Auth/Login';
+import DetailProduct from './pages/DetailProduct';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={<Login/>}/> */}
+        <Route path="/detail-product" element={<DetailProduct/>}/>
       </Routes>
     </BrowserRouter>
   );
