448a330a15656dcb046321547bd4c0eb745696b2
dindachyfr <dindachyfr@gmail.com>
2022-01-24 00:26:50 +0700

    get data from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect, useContext } from 'react'
 import { useNavigate, Link, useSearchParams, Navigate  } from 'react-router-dom'
 import axios from 'axios'
@@ -5,24 +6,13 @@ import CardProduct from "../../components/CardProduct";
 import HomeCarousel from "../../components/HomeCarousel";
 import MainHome from "../../components/MainHome";
 import Navbar from "../../components/Navbar";
+import { productContext } from '../../Context/ProductContext';
 
 const HomePage = () => {
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   const navigate = useNavigate()
-  useEffect(() => {
-    axios({
-      baseURL : `https://blanja-app-2codeblue.herokuapp.com/`,
-      method : 'GET',
-      url : `/products`
-    })
-    .then((res) => {
-      const result = res.data.data
-      setProducts(result)
-    })
-    .catch((err) => {
-      console.log(err.message)
-    })
-  }, []);
+  const {products, setProducts} = useContext(productContext)
+
 
   return (
     <div className="">
@@ -37,8 +27,12 @@ const HomePage = () => {
           <div className="card-container d-flex flex-wrap flex-fill justify-content-center">
             {
               products.map((product) => {
-                return <CardProduct key={product.id} image={product.image1} name={product.name}
-                price={product.price} store_name={product.store_name} onClick={() => navigate(`/detail-product/${product.id}`)} />
+                return <CardProduct key={product? product.id : `id`} 
+                image={product? product.image1 : `Loading`} 
+                name={product? product.name: `Loading`}
+                price={product? product.price : `Loading`} 
+                store_name={product? product.store_name : `Loading`} 
+                onClick={() => navigate(`/detail-product/${product.id}`)} />
               })
             }
           </div>
