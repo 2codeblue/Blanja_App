a3942c24a16d5c63c9c76021b38c06137e68dcb2
Juan Tombeng <juantombeng03@gmail.com>
2022-01-25 03:25:20 +0700

    fixing product context

--- src/Context/ProductContext.js
+++ src/Context/ProductContext.js
@@ -1,28 +1,26 @@
-import React, { createContext, useEffect, useState } from 'react';
-import axios from "axios"
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
-export const productContext = createContext(null)
+export const productContext = createContext(null);
 
-const ProductContext = ({children}) => {
-    const [products, setProducts] = useState([])
-    
-    useEffect(()=>{
-        axios.get(`https://blanja-app-2codeblue.herokuapp.com/products`)
-        .then((res)=>{
-          const result = res.data.data
-          console.log(result);
-          setProducts(result)
+const ProductContext = ({ children }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://blanja-app-2codeblue.herokuapp.com/products`)
+      .then((res) => {
+        const result = res.data.data;
+        setProducts(result);
       })
-      .catch((err)=>{
-          console.log(err.response);
-      })
-    }, [])
-    console.log(products);
-    
-      return (
-  <productContext.Provider value={{products, setProducts}}>
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, []);
+  return (
+    <productContext.Provider value={{ products, setProducts }}>
       {children}
-  </productContext.Provider>
+    </productContext.Provider>
   );
 };
 
