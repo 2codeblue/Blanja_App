3b24fc908f17d51fdb35ec0fa49fa65ae4ed5c9a
Juan Tombeng <juantombeng03@gmail.com>
2022-01-26 18:58:33 +0700

    user profile form error

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,180 +1,196 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Button from '../../components/Button';
-import Input from '../../components/Input';
-import Navbar from '../../components/Navbar';
-import styles from './profile.module.css'
-import ProfImg from '../../assets/img/prodPic.svg'
-import Sidebar from '../../components/Sidebar';
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import Navbar from "../../components/Navbar";
+import styles from "./profile.module.css";
+import ProfImg from "../../assets/img/prodPic.svg";
+import Sidebar from "../../components/Sidebar";
+import { userContext } from "../../Context/UserContext";
 
 const Profile = () => {
-    const [form, setForm] = useState({ name: "", email: "", phone_number: "" });
-    const [gender, setGender] = useState("");
-    const [user, setUser]= useState(null)
-    const userFromLS = JSON.parse(localStorage.getItem("userId"))
+  const [form, setForm] = useState({ name: "", email: "", phone_number: "" });
+  const [gender, setGender] = useState("");
+  const { user, setUser } = useContext(userContext);
+  const userId = JSON.parse(localStorage.getItem("userId"));
 
-    useEffect(()=>{
-      // console.log(userFromLS);
-      axios({
-        baseURL: `${process.env.REACT_APP_URL_BACKEND}`,
-        method : 'GET',
-        url : `/users/customer/${userFromLS}`
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  // const handleCustomerPU = () =>{
+  //     axios.put(`${process.env.REACT_APP_URL_BACKEND}/users/customer/${userFromLS}`,{
+  //         name: form.name,
+  //         email: form.email,
+  //         phone_number: form.phone_number,
+  //         gender: gender
+  //     }).then((res)=>{
+  //         setUser({...user,
+  //             name: form.name,
+  //             email: form.email,
+  //             phone_number: form.phone_number,
+  //             gender: gender})
+  //     })
+  // }
+
+  const handleCustomerPU = () => {
+    axios({
+      baseURL: `${process.env.REACT_APP_URL_BACKEND}`,
+      data: {
+        name: form.name,
+        email: form.email,
+        phone_number: form.phone_number,
+        gender: gender,
+        DOB : user.DOB,
+        profile_picture : null
+      },
+      method: `PUT`,
+      url: `/users/customer/${userId}`,
     })
-      .then((res) => {
-        const result = res.data.result;
-        console.log(result);
-        setUser(result);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    },[])
+    .then((res) => {
+        console.log(res.data.data)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+  };
 
-    const handleChange = (e) => {
-        setForm({ ...form, [e.target.name]: e.target.value });
-        setGender(e.target.value);
-      };
-      console.log(gender);
+  const handleGender = (e) => {
+    console.log(e.target.value);
+  };
 
-    const handleCustomerPU = () =>{
-        axios.put(`${process.env.REACT_APP_URL_BACKEND}/users/customer/${userFromLS}`,{
-            name: form.name,
-            email: form.email,
-            phone_number: form.phone_number,
-            gender: gender
-        }).then((res)=>{
-            setUser({...user, 
-                name: form.name,
-                email: form.email,
-                phone_number: form.phone_number,
-                gender: gender})
-        })
-    }
-
+  console.log(user);
   return (
-    <main className={`containder-fluid bg-white ${styles.con} d-flex flex-column`}>
-      <Navbar/>
+    <main
+      className={`containder-fluid bg-white ${styles.con} d-flex flex-column`}
+    >
+      <Navbar />
       <div className="d-flex w-100 flex-fill">
-          <Sidebar/>
-          {/*  <div className={`w-25 sidebar`}>
-                  sidebar
-          </div> */}
-          <div className={`w-75 bg-light profileBox pt-3`}>
-              <div className={`${styles.profilebox} mt-5 ms-5 p-3 bg-white`}>
-                  <div className={`profilebox-wrapper h-100`}>
-                    <h6 className="fw-bold">My Profile
-                    <br />
-                    <span className="text-secondary fw-light py-3">Manage your profile information</span></h6>
-                    <hr className='mt-3'/>
-                    <div className="d-flex profile-forms-wrapper">
-                        <div className="w-75">
-                        <div className="w-100 d-flex justify-content-between px-5">
-                                <label className="input-name mt-2" htmlFor="Name">
-                                    Name
-                                </label>
-                                <Input
-                                    name="name"
-                                    onChange= {handleChange}
-                                    value={form.name}
-                                    className={`${styles.inputForm}`}
-                                    type="text"
-                                    id="Name"
-                                    placeholder={user? user.name : `Nanang Ismail`}
-                                />
-                        </div>
-                        <div className="w-100 d-flex justify-content-between mt-3 px-5">
-                                <label className="input-name mt-2" htmlFor="Name">
-                                    Email
-                                </label>
-                                <Input
-                                    name="email"
-                                    onChange= {handleChange}
-                                    value={form.email}
-                                    className={`${styles.inputForm}`}
-                                    type="email"
-                                    id="email"
-                                    placeholder={user? user.email : `nangis@gmail.com`}
-                                />
-                        </div>
+        <Sidebar />
+        <div className={`w-75 bg-light profileBox pt-3`}>
+          <div className={`${styles.profilebox} mt-5 ms-5 p-3 bg-white`}>
+            <div className={`profilebox-wrapper h-100`}>
+              <h6 className="fw-bold">
+                My Profile
+                <br />
+                <span className="text-secondary fw-light py-3">
+                  Manage your profile information
+                </span>
+              </h6>
+              <hr className="mt-3" />
+              <div className="d-flex profile-forms-wrapper">
+                <div className="w-75">
+                  <div className="w-100 d-flex justify-content-between px-5">
+                    <label className="input-name mt-2" htmlFor="Name">
+                      Name
+                    </label>
+                    <Input
+                      name="name"
+                      onChange={handleChange}
+                      value={form.name}
+                      className={`${styles.inputForm}`}
+                      type="text"
+                      id="Name"
+                      placeholder={user ? user.name : `Loading User Name`}
+                    />
+                  </div>
+                  <div className="w-100 d-flex justify-content-between mt-3 px-5">
+                    <label className="input-name mt-2" htmlFor="Name">
+                      Email
+                    </label>
+                    <Input
+                      name="email"
+                      onChange={handleChange}
+                      value={form.email}
+                      className={`${styles.inputForm}`}
+                      type="email"
+                      id="email"
+                      placeholder={
+                        user ? user.email : `Loading User Email account`
+                      }
+                    />
+                  </div>
 
-                        <div className="w-100 d-flex justify-content-between mt-3 px-5">
-                                <label className="input-name mt-2" htmlFor="Name">
-                                    Phone number
-                                </label>
-                                <Input
-                                    name="phone_number"
-                                    onChange= {handleChange}
-                                    value={form.phone_number}
-                                    className={`${styles.inputForm}`}
-                                    type="number"
-                                    id="phone_number"
-                                    placeholder={user? user.phone_number : `0812345678900`}
-                                />
-                        </div>
+                  <div className="w-100 d-flex justify-content-between mt-3 px-5">
+                    <label className="input-name mt-2" htmlFor="Name">
+                      Phone number
+                    </label>
+                    <Input
+                      name="phone_number"
+                      onChange={handleChange}
+                      value={form.phone_number}
+                      className={`${styles.inputForm}`}
+                      type="number"
+                      id="phone_number"
+                      placeholder={
+                        user ? user.phone_number : `Loading Phone Number`
+                      }
+                    />
+                  </div>
 
-                        <div className="w-100 d-flex justify-content-between mt-3 px-5">
-                                <label className="input-name mt-2" htmlFor="Name">
-                                    Gender
-                                </label>
-                                <div className='d-flex mt-2 ps-5'>
-                                <Input 
-                                    className={`${styles.inputForm1} mt-1`}
-                                    onChange= {handleChange}
-                                    name="gender"
-                                    type="radio"
-                                    value="male"
-                                    id="gender"
-                                /> Male
-                                </div>
-                                <div className='d-flex mt-2'>
-                                <Input
-                                    className={`${styles.inputForm1} mt-1`}
-                                    onChange= {handleChange}
-                                    name="gender"
-                                    type="radio"
-                                    value="female"
-                                    id="gender"
-                                /> Female
-                                </div>
-                        </div>
-                        <div className="w-100 d-flex justify-content-between mt-3 px-5">
-                            <label className="input-name mt-2" htmlFor="Name">
-                                Date of birth
-                            </label>
-                            <Input
-                                name="DOB"
-                                className={`${styles.inputForm}`}
-                                type="text"
-                                id="DOB"
-                                placeholder={user? user.DOB.toLocaleDateString() : `31/09/1988`}
-                                disabled
-                            />
-                </div>
-                        <Button 
-                            className={`${styles.lowerButtons} ${styles.redButton} bg-primary ms-5 mt-3`}
-                            onClick={handleCustomerPU}
-                            >
-                            Save
-                            </Button>
-
-
-                        </div>
-                        <div className="w-25 profpic-setting">
-                            <div className={`${styles.wrapper} w-100 d-flex flex-column justify-content-center`}>
-                                <img src={ProfImg} className={`${styles.profImg}`} alt="" />
-                                <Button
-                                    className={`${styles.lowerButtons1} ${styles.redButton1} bg-transparent mt-3`}
-                                    >
-                                    Select Image
-                                    </Button>
-                            </div>
-                        </div>
+                  <div className="w-100 d-flex justify-content-between mt-3 px-5">
+                    <label className="input-name mt-2" htmlFor="Name">
+                      Gender
+                    </label>
+                    <div className="d-flex mt-2 ps-5">
+                      <Input
+                        className={`${styles.inputForm1} mt-1`}
+                        onChange={handleGender}
+                        name="male"
+                        type="radio"
+                        value="Male"
+                      />{" "}
+                      Male
+                    </div>
+                    <div className="d-flex mt-2">
+                      <Input
+                        className={`${styles.inputForm1} mt-1`}
+                        onChange={handleGender}
+                        name="female"
+                        type="radio"
+                        value="Female"
+                      />{" "}
+                      Female
                     </div>
                   </div>
+                  <div className="w-100 d-flex justify-content-between mt-3 px-5">
+                    <label className="input-name mt-2" htmlFor="Name">
+                      Date of birth
+                    </label>
+                    <Input
+                      name="DOB"
+                      className={`${styles.inputForm}`}
+                      type="text"
+                      id="DOB"
+                      placeholder={user ? user.DOB : `Loading`}
+                      disabled
+                    />
+                  </div>
+                  <Button
+                    className={`${styles.lowerButtons} ${styles.redButton} bg-primary ms-5 mt-3`}
+                    onClick={handleCustomerPU}
+                  >
+                    Save
+                  </Button>
+                </div>
+                <div className="w-25 profpic-setting">
+                  <div
+                    className={`${styles.wrapper} w-100 d-flex flex-column justify-content-center`}
+                  >
+                    <img src={ProfImg} className={`${styles.profImg}`} alt="" />
+                    <Button
+                      className={`${styles.lowerButtons1} ${styles.redButton1} bg-transparent mt-3`}
+                    >
+                      Select Image
+                    </Button>
+                  </div>
+                </div>
               </div>
+            </div>
           </div>
+        </div>
       </div>
-      </main>    
+    </main>
   );
 };
 
